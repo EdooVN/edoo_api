e78328d9712f107eea7cad6fe81019b80ed8ad3c
TooNies1810 <minhnt_58@vnu.edu.vn>
2016-09-15 00:55:03 +0700

    test token

--- app/helpers/commonVLs.js
+++ app/helpers/commonVLs.js
@@ -4,6 +4,12 @@
  * test 8h hết hạn token
  * @type {number}
  */
-module.exports.timeExtension = 28800000;
+// module.exports.timeExtension = 28800000;
+
+/**
+ * 2 phut
+ * @type {number}
+ */
+module.exports.timeExtension = 120000;
 
 module.exports.page_size = 8;

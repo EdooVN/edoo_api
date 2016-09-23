aa1b1751130ae1c04d01ad2ada64f3e7d628f58c
TooNies1810 <minhnt_58@vnu.edu.vn>
2016-09-23 09:31:04 +0700

    email reset pass

--- app/services/EmailService.js
+++ app/services/EmailService.js
@@ -6,11 +6,16 @@ const API_KEY_SENDGRID = config('API_KEY_SENDGRID', '');
 const sg = require('sendgrid')(API_KEY_SENDGRID);
 
 module.exports.sendRefreshPass = function (userDetail, token, cb) {
-    let title = 'Reset password';
+    let title = '[Edoo] Hướng dẫn khôi phục mật khẩu';
 
     let urlResetPass = 'http://uetf.me/#/reset-pass/' + token;
     // let content_html = '<a href="' + urlResetPass + '" target="_blank">' + '</a>';
-    let content_html = urlResetPass;
+    let content_html =
+        '<p>' + 'Xin chào ' + userDetail.name + ',' + '</p>'
+        + '<p>' + 'Bạn đã gửi một yêu cầu khôi phục mật khẩu cho tài khoản Edoo của mình. Để hoàn tất quá trình, vui lòng bấm vào đường dẫn dưới đây:' + '</p>'
+        + '<p>' + '<a href="' + urlResetPass + '">' + 'Khôi phục mật khẩu' + '</a>' + '</p>'
+        + '<p>' + 'Nếu bạn không muốn khôi phục mật khẩu, hãy bỏ qua email này.' + '</p>'
+        + '<p>' + 'Hỗ trợ Edoo' + '</p>';
     sendEmail(userDetail.email, title, content_html, cb);
 };
 
@@ -22,7 +27,7 @@ function sendEmail(email_user, title, content_html, callback) {
     var from_email = new helper.Email(from);
     var to_email = new helper.Email(to);
     var subject = title;
-    var content = new helper.Content('text/plain', content_html);
+    var content = new helper.Content('text/html', content_html);
     var mail = new helper.Mail(from_email, subject, to_email, content);
 
     var request = sg.emptyRequest({
@@ -36,7 +41,7 @@ function sendEmail(email_user, title, content_html, callback) {
         // console.log(response.body);
         // console.log(response.headers);
 
-        if (response.statusCode == 202){
+        if (response.statusCode == 202) {
             callback(false);
         } else {
             callback(true);
